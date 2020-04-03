c0315dad2329ea75a57163abdbb6c80559e24113
JaeHyukSim <jaehyukawot@gmail.com>
2020-04-03 16:08:14 +0900

    1

---
+++ tripProject02/src/com/sist/model/tmpModel.java
@@ -0,0 +1,17 @@
+package com.sist.model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+@Controller
+public class tmpModel {
+
+	/*@RequestMapping("views/template/main/staying.do")
+	public String tmpMethod(HttpServletRequest request,HttpServletResponse response){
+		request.setAttribute("msg", "숙소입니다");
+		return "index.jsp?mode=4";
+	}*/
+}
